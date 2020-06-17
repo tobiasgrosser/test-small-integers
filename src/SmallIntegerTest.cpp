430bb1220feb61a8461e453e789445fbe8b1ff25
Tobias Grosser <tobias@grosser.es>
2020-06-17 07:47:33 +0000

    Add first set of test cases

--- src/SmallIntegerTest.cpp
+++ src/SmallIntegerTest.cpp
@@ -1,5 +1,24 @@
 #include "gtest/gtest.h"
 #include "SmallInteger.h"
+#include <type_traits>
+
+typedef ::testing::Types<
+	SmallInteger_sioimath,
+	SmallInteger_1,
+	SmallInteger_2,
+	SmallInteger_3> TestTypes;
+
+template <class T>
+class TestSI : public testing::Test {
+
+};
+
+TYPED_TEST_SUITE(TestSI, TestTypes);
+
+TYPED_TEST(TestSI, CreateZero) {
+  TypeParam Zero;
+  EXPECT_TRUE(Zero.isZero());
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
